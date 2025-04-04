1fff069b13d396b9aa1b763be413120a5153caa8
Vladimir Karpov <w-karpov@mail.ru>
2025-04-04 07:48:30 +0300

    main

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	// Set the flags for the logging package to give us the filename in the logs
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	log.Println("starting server...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprintln(w, `Hello, visitor!`)
+	})
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprintln(w, `Hello, test12 visitor!`)
+	})
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
